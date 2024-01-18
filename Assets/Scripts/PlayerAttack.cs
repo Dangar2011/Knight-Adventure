c8a50c0a46cee4f7ee142284256ac9d270858a2f
Đinh Văn Đảng <1951060586@e.tlu.edu.vn>
2024-01-19 02:54:37 +0700

    fix PlayerAttack

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -9,29 +9,40 @@ public class PlayerAttack : MonoBehaviour
     private float damage;
     [SerializeField] private BoxCollider2D coll;
     private EnemyLife enemyLife;
-    private StoneBoss stoneBoss;
+
     void Start()
     {
         damage = GetComponent<Player>().GetPlayerDamage();
     }
-    private bool EnemyInsight()
-    {
-        RaycastHit2D hit = Physics2D.BoxCast(coll.bounds.center + transform.right * attackRange * transform.GetComponent<PlayerMovement>().facingDirection,
-            new Vector3(coll.bounds.size.x * attackRange, coll.bounds.size.y, coll.bounds.size.z), 0.1f, Vector2.right, 0, enemyLayer);
-        if (hit.collider != null)
-        {
-            enemyLife = hit.transform.GetComponent<EnemyLife>();
+    //private bool EnemyInsight()
+    //{
+    //    RaycastHit2D hit = Physics2D.BoxCast(coll.bounds.center + transform.right * attackRange * transform.GetComponent<PlayerMovement>().facingDirection,
+    //        new Vector3(coll.bounds.size.x * attackRange, coll.bounds.size.y, coll.bounds.size.z), 0.1f, Vector2.right, 0, enemyLayer);
+    //    if (hit.collider != null)
+    //    {
+    //        enemyLife = hit.transform.GetComponent<EnemyLife>();
 
-        }
-        return hit.collider != null;
-    }
+    //    }
+    //    return hit.collider != null;
+    //}
     private void DamageEnemy()
     {
+        Collider2D[] hitEnemies = Physics2D.OverlapBoxAll(coll.bounds.center + transform.right * attackRange * transform.GetComponent<PlayerMovement>().facingDirection,
+        new Vector3(coll.bounds.size.x * attackRange, coll.bounds.size.y, coll.bounds.size.z), 0.1f, enemyLayer);
 
-            if (EnemyInsight() && !enemyLife.IsDead())
+        foreach (Collider2D enemyCollider in hitEnemies)
+        {
+            EnemyLife enemyLife = enemyCollider.GetComponent<EnemyLife>();
+            if (enemyLife != null && !enemyLife.IsDead())
             {
                 enemyLife.TakeDamage(damage);
             }
+        }
+
+        //if (EnemyInsight() && !enemyLife.IsDead())
+        //{
+        //    enemyLife.TakeDamage(damage);
+        //}
 
     }
     private void OnDrawGizmos()
